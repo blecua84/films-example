afb66e798af4390cb2844b5b45a947af6d1b8cfb
blecua84 <josejavier.blecua@gmail.com>
2017-10-16 19:51:45 +0100

    chore(protractor): Update configuration.

--- config/protractor.conf.js
+++ config/protractor.conf.js
@@ -1,14 +1,18 @@
 //protractor.conf.js
+
+var cucumberReportDirectory = 'protractor-report';
+var jsonReportFile = cucumberReportDirectory + '/cucumber_report.json';
+
 exports.config = {
+    keepAlive: true,
     seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
     getPageTimeout: 60000,
     allScriptsTimeout: 500000,
     framework: 'custom',
-    directConnect: true,
     // path relative to the current config file
     frameworkPath: require.resolve('protractor-cucumber-framework'),
     capabilities: {
-      'browserName': 'chrome'
+      browserName: 'chrome',
     },
   
     // Spec patterns are relative to this directory.
@@ -19,16 +23,27 @@ exports.config = {
     baseURL: 'http://localhost:4200/',
   
     cucumberOpts: {
+      format: 'json:./' + jsonReportFile,
       require: '../e2e/steps/*.steps.js',
-      //tags: [],
-      //strict: true,
-      //compiler: []
     }, 
+
+    ignoreUncaughtExceptions: true,
+    untrackOutstandingTimeouts: true,
+
+    onCleanUp: () => {
+      var CucumberHtmlReport = require('cucumber-html-report');
+
+      return CucumberHtmlReport.create({
+          source: jsonReportFile,
+          dest: cucumberReportDirectory,
+          title: 'Films BDD Reporter',
+          component: new Date().toString()
+      }).then(console.log).catch(console.log);
+    },
 
     onPrepare: () => {
       browser.manage().window().maximize();
-      // implicit and page load timeouts
-      browser.manage().timeouts().pageLoadTimeout(40000);
-      browser.manage().timeouts().implicitlyWait(25000);
+      browser.ignoreSynchronization = true
+      browser.waitForAngular();
     }
   };
